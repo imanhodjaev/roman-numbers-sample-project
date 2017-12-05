3d56c46cc38fd8ca4f697f83484deb16bc5ca58b
Sultan Iman <sultan.imanhodjaev@gmail.com>
2017-12-05 14:08:21 +0100

    feat(solution) implement solution

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,13 +1,36 @@
 # -*- coding: utf-8 -*-
-
+from collections import OrderedDict
 
 __all__ = ['convert']
+
+PAIRS = OrderedDict({
+    1000: 'M',
+    900: 'CM',
+    500: 'D',
+    400: 'CD',
+    100: 'C',
+    90: 'XC',
+    50: 'L',
+    40: 'XL',
+    10: 'X',
+    9: 'IX',
+    5: 'V',
+    4: 'IV',
+    1: 'I',
+})
 
 
 def convert(number: int) -> str:
     """Convert decimal (arabic) numbers to roman numerals"""
     check_input(number)
-    return ''
+    result = ''
+
+    for num in PAIRS.keys():
+        count, rem = divmod(number, num)
+        result += PAIRS[num] * count
+        number -= num * count
+
+    return result
 
 
 def check_input(number: int):
