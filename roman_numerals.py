b8013d7c2a9c90e4e6bbedbb15e0a5f5800a7d6e
Sultan Iman <sultan.imanhodjaev@gmail.com>
2017-12-05 12:21:30 +0100

    test(converter) Add more tests to check user input

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,5 +1,18 @@
 # -*- coding: utf-8 -*-
 
 
-def convert(number):
-    """Convert arabic numbers to roman numerals"""
+__all__ = ['convert']
+
+
+def convert(number: int) -> str:
+    """Convert decimal (arabic) numbers to roman numerals"""
+    check_input(number)
+    return ''
+
+
+def check_input(number: int):
+    if type(number) != int:
+        raise ValueError('Input number must be integer')
+
+    if number <= 0:
+        raise ValueError('Input number must be positive integer')
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -8,9 +8,20 @@ def test_it_works():
     assert 1 == 1
 
 
-def test_it_fail_on_zero_or_negative_number():
+def test_it_fails_on_zero_or_negative_number():
     with pytest.raises(ValueError):
         convert(0)
 
     with pytest.raises(ValueError):
         convert(-1)
+
+
+def test_it_fails_when_input_type_is_not_an_int_number():
+    with pytest.raises(ValueError):
+        convert({})
+
+    with pytest.raises(ValueError):
+        convert(1.0)
+
+    with pytest.raises(ValueError):
+        convert('random string')
