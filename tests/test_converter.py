1aef4741eeedc75287e2662c330b0df1568375e3
Sultan Iman <sultan.imanhodjaev@gmail.com>
2017-12-05 12:28:49 +0100

    test(converter) Add test case to test basic roman number literals and their expected decimal number

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -25,3 +25,18 @@ def test_it_fails_when_input_type_is_not_an_int_number():
 
     with pytest.raises(ValueError):
         convert('random string')
+
+
+def test_it_works_and_converts_basic_roman_numerals():
+    """Given table of basic literals, test if converting works correctly
+    | Symbol |  I  |  V  |  X   |   L   |   C   |   D   |   M   |
+    |--------|-----|-----|------|-------|-------|-------|-------|
+    | Value  |  1  |  5  |  10  |   50  |  100  |  500  | 1000  |
+    """
+    assert convert(1) == 'I'
+    assert convert(5) == 'V'
+    assert convert(10) == 'X'
+    assert convert(50) == 'L'
+    assert convert(100) == 'C'
+    assert convert(500) == 'D'
+    assert convert(1000) == 'M'
