1a83e07f34b5a2543fb979227112d5fb79e10768
Sultan Iman <sultan.imanhodjaev@gmail.com>
2017-12-05 11:36:18 +0100

    test(converter) Add tests for 0 or negative numbers

--- roman_numerals.py
+++ roman_numerals.py
@@ -1,2 +1,5 @@
+# -*- coding: utf-8 -*-
+
+
 def convert(number):
     """Convert arabic numbers to roman numerals"""
--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,2 +1,16 @@
+# -*- coding: utf-8 -*-
+import pytest
+
+from roman_numerals import convert
+
+
 def test_it_works():
     assert 1 == 1
+
+
+def test_it_fail_on_zero_or_negative_number():
+    with pytest.raises(ValueError):
+        convert(0)
+
+    with pytest.raises(ValueError):
+        convert(-1)
