d64c0ba2472982d061cf3afb2c0b19fd2393ea2b
Sultan Iman <sultan.imanhodjaev@gmail.com>
2017-12-05 14:07:44 +0100

    text(converter) add test cases for different numbers

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -40,3 +40,15 @@ def test_it_works_and_converts_basic_roman_numerals():
     assert convert(100) == 'C'
     assert convert(500) == 'D'
     assert convert(1000) == 'M'
+
+
+def test_it_work_with_other_numbers():
+    assert convert(1776) == 'MDCCLXXVI'     # Date written on the book held by the Statue of Liberty
+    assert convert(1954) == 'MCMLIV'        # As in the trailer for the movie The Last Time I Saw Paris
+    assert convert(1990) == 'MCMXC'         # Used as the title of musical project Enigma's debut album MCMXC a.D., named after the year of its release.
+    assert convert(2014) == 'MMXIV'         # The year of the games of the XXII (22nd) Olympic Winter Games (in Sochi)
+    assert convert(2017) == 'MMXVII'
+    assert convert(2018) == 'MMXVIII'
+    assert convert(717) == 'DCCXVII'
+    assert convert(777) == 'DCCLXXVII'
+    assert convert(888) == 'DCCCLXXXVIII'
